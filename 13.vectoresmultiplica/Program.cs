07239d94b44afab1bea2029074a0cf7ca5eacded
Erasmo Reyes Ortiz <mundozombie1995@hotmail.com>
2019-12-08 20:13:23 -0600

    actualizacion

---
+++ 13.vectoresmultiplica/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _13.vectoresmultiplica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vectorA, vectorB, vectorC;
+
+            vectorA = new int[20];
+            vectorB = new int[20];
+            vectorC = new int[20];
+
+            leerDatos(vectorA,"A");
+            leerDatos(vectorB,"B");
+
+            multiplicar(vectorA, vectorB, vectorC);
+
+            mostrarDatos(vectorA, "A");
+            mostrarDatos(vectorB, "B");
+            mostrarDatos(vectorC, "C");
+        }
+
+        static void multiplicar(int[] vectorA, int[] vectorB, int[] vectorC){
+
+            for (int i = 0; i < vectorA.Length; i++)
+            {
+                vectorC[i] = vectorA[i] * vectorB[vectorB.Length-1 -i ];
+            }
+        }
+
+        static void leerDatos(int[] vector, string a){
+            Console.WriteLine($"Ingrese los datos del vector {a}");
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine($"Ingrese valor {i+1}");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void mostrarDatos(int[] vector, string a){
+             Console.WriteLine($"Datos del vector {a}");
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"{vector[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
